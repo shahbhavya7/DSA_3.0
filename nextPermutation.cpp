d29976cf04e7ee9e61eb444f941dcb32dbde203a
Bhavya <bhavya.shah@mitwpu.edu.in>
2026-01-09 11:44:03 +0530

    Refactor nextPermutation implementation for improved readability and consistency

--- nextPermutation.cpp
+++ nextPermutation.cpp
@@ -1,66 +1,55 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    void nextPermutation(vector<int>& nums) {
-       next_permutation(nums.begin(),nums.end()) ;
-
+    void nextPermutation(vector<int> &nums)
+    {
+        next_permutation(nums.begin(), nums.end());
     }
 };
 
-class SolutionOptimal {
+class SolutionOptimized
+{ 
 public:
-    // Function to find next permutation
-    void nextPermutation(vector<int>& nums) {
-        // Set index to -1
-        int index = -1;
+    void nextPermutation(vector<int> &nums){
+        int n  = nums.size();
+        int index  = 1;
 
-        // Find the first decreasing element from end
-        for (int i = nums.size() - 2; i >= 0; i--) {
-            // If a smaller element found
-            if (nums[i] < nums[i + 1]) {
-                // Store index
-                index = i;
+        for(int i=n-2;i>=0;i--){ // Find the first decreasing element from end
+            if(nums[i]<nums[i+1]){
+                index = i; // to capture the index of the first decreasing element
                 break;
             }
         }
-
-        // If no such index found
-        if (index == -1) {
-            // Reverse the entire array
-            reverse(nums.begin(), nums.end());
-            return;
-        }
-
-        // Find element just greater than nums[index]
-        for (int i = nums.size() - 1; i > index; i--) {
-            // Swap the two
-            if (nums[i] > nums[index]) {
-                swap(nums[i], nums[index]);
+        for (int i = n-1; i > index; i--) {  // find the just larger element to swap with
+            if(nums[i]>nums[index]){
+                swap(nums[i],nums[index]);
                 break;
             }
         }
-
-        // Reverse the part after index
-        reverse(nums.begin() + index + 1, nums.end());
+        reverse(nums.begin()+index+1,nums.end()); // reverse the elements after the index to maintain the smallest order
     }
+
 };
+    int main()
+    {
+        Solution sol;
+        vector<int> nums = {1,2,3,6,5,4};
+        sol.nextPermutation(nums);
+        for (int num : nums)
+        {
+            cout << num << " ";
+        }
+        SolutionOptimized solOpt;
+        nums = {1,2,3,6,5,4};
+        solOpt.nextPermutation(nums);
+        cout << endl;
+        for (int num : nums)
+        {
+            cout << num << " ";
+        }
 
-int main(){
-    Solution sol;
-    vector<int> nums = {1, 2, 3};
-    sol.nextPermutation(nums);
-    for(int num : nums){
-        cout << num << " ";
+        return 0;
     }
-    SolutionOptimal solOpt;
-    nums = {1, 2, 3};
-    solOpt.nextPermutation(nums);
-    cout << endl;
-    for(int num : nums){
-        cout << num << " ";
-    }
-    
-    return 0;
-}
