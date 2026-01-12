30405cff9dcc66d91fea41c7c0683ec993541607
shahbhavya7 <146073814+shahbhavya7@users.noreply.github.com>
2026-01-12 19:34:35 +0530

    Find Duplicate - Optimized Method

--- findDuplicate.cpp
+++ findDuplicate.cpp
@@ -42,14 +42,30 @@ public:
 
 class SolutionOptimal
 {
-    public:
-        int findDuplicate(vector<int> &nums)
+public:
+    int findDuplicate(vector<int> &nums)
     {
         int n = nums.size();
         int slow = nums[0];
         int fast = nums[0];
+        // we reimagine the array as a linked list where each index becomes node and value at that index becomes pointer to next node
+        // Phase 1: Finding the intersection point in the cycle
+        do
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
 
-        
+        // Phase 2: Finding the entrance to the cycle
+        fast = nums[0];
+
+        // move both pointers at same speed, they will meet at the entrance of the cycle
+        while (slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow; // or return fast, both are at the entrance of the cycle
     }
 };
 
@@ -63,6 +79,11 @@ int main()
     SolutionBetter solBetter;
     nums = {3, 1, 3, 4, 2};
     duplicate = solBetter.findDuplicate(nums);
+    cout << "Duplicate number is: " << duplicate << endl;
+
+    SolutionOptimal solOptimal;
+    nums = {3, 1, 3, 4, 2};
+    duplicate = solOptimal.findDuplicate(nums);
     cout << "Duplicate number is: " << duplicate << endl;
 
     return 0;
