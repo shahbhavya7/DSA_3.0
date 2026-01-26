eda909592176e4d4e5d914b4119a61db890aea63
shahbhavya7 <146073814+shahbhavya7@users.noreply.github.com>
2026-01-26 20:38:19 +0530

    Reverse String - Optimal

--- Strings/reverseString.cpp
+++ Strings/reverseString.cpp
@@ -26,13 +26,30 @@ public:
         return result;
     }
 
+    string reverseWordsOptimal(string s){
+        int n = s.length();
+        string result = "";
+
+        reverse(s.begin(), s.end()); // reverse the entire string
+
+        for (int i = 0; i < n; i++) {
+            string word = "";
+            while(i<n && s[i]!=' '){
+                word += s[i];
+                i++;
+            }
+            reverse(word.begin(), word.end()); // reverse the individual word
+            if(word.length() > 0) result += " " + word; // add the word to result with a space
+        }
+        return result.substr(1) ; // remove the trailing space at the beginning
+    }
 };
 
 int main()
 {
     Solution sol;
     string s = "the sky is blue";
-    string result = sol.reverseWords(s);
+    string result = sol.reverseWordsOptimal(s);
     cout << "Reversed Words: " << result << endl;
 
     return 0;
