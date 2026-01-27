bbb495f684f3dfd39af67344cf246e02f6958047
shahbhavya7 <146073814+shahbhavya7@users.noreply.github.com>
2026-01-27 20:01:40 +0530

    Roman to Int

---
+++ Strings/romanToInt.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        int sum = 0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='I'){
+                if(i+1<s.size() && (s[i+1]=='V' || s[i+1]=='X')){ // if next char is V or X and i+1 is within bounds
+                    sum -= 1;
+                }else{
+                    sum += 1;
+                }
+            }else if(s[i]=='V'){
+                sum += 5;
+            }
+            else if(s[i]=='X'){
+                if(i+1<s.size() && (s[i+1]=='L' || s[i+1]=='C')){
+                    sum -= 10;
+                }else{
+                    sum += 10;
+                }
+            }else if(s[i]=='L'){
+                sum += 50;
+            }else if(s[i]=='C'){
+                if(i+1<s.size() && (s[i+1]=='D' || s[i+1]=='M')){
+                    sum -= 100;
+                }else{
+                    sum += 100;
+                }
+            }else if(s[i]=='D'){
+                sum += 500;
+            }else if(s[i]=='M'){
+                sum += 1000;
+            }
+            else{
+                return -1; // invalid character
+            }
+        }
+        return sum;
+    }
+};
+
+int main(){
+    Solution sol;
+    string s = "MCMXCIV";
+    int result = sol.romanToInt(s);
+    cout << "Integer value of Roman numeral " << s << " is: " << result << endl;
+    return 0;
+}
