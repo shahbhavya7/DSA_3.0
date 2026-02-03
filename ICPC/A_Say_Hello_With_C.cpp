750543ebf830de203be41eb44bca1850292ba3c7
shahbhavya7 <146073814+shahbhavya7@users.noreply.github.com>
2026-02-03 19:51:32 +0530

    A_Say_Hello_With_C  + B_Basic_Data_Types functions

---
+++ ICPC/A_Say_Hello_With_C.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+void solve()
+{
+    string s;
+    cin >> s;
+    cout << "Hello, " << s;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1;
+    // cin >> t; // Uncomment this line if there are multiple test cases
+    while (t--)
+        solve();
+
+    return 0;
+}
