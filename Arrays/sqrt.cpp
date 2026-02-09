20ee65ea912f186646bc1b1f3024c971b71a1a39
shahbhavya7 <146073814+shahbhavya7@users.noreply.github.com>
2026-02-09 23:06:04 +0530

    PrimeFactors

---
+++ Arrays/sqrt.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    vector<int>PrimeFactors(int n){
+        vector<int> factors;
+        for(int i=2; i*i<=n; i++){
+            while(n%i==0){
+                factors.push_back(i);
+                n/=i;
+            }
+        }
+        if(n>1) factors.push_back(n);
+        return factors;
+    }
+
+    int mySqrt(int x) {
+        vector<int> factors = PrimeFactors(x);
+        int result = 1;
+    }
+};
+
+int main(){
+
+    return 0;
+}
