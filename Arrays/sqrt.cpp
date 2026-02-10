71a7e7f144753f4d90eeff41b48805f993fabe38
shahbhavya7 <146073814+shahbhavya7@users.noreply.github.com>
2026-02-10 23:48:36 +0530

    Refactor formatting and style in sqrt.cpp for consistency

--- Arrays/sqrt.cpp
+++ Arrays/sqrt.cpp
@@ -1,28 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-
-    vector<int>PrimeFactors(int n){
+    vector<int> PrimeFactors(int n)
+    {
         vector<int> factors;
-        for(int i=2; i*i<=n; i++){
-            while(n%i==0){
+        for (int i = 2; i * i <= n; i++)
+        {
+            while (n % i == 0)
+            {
                 factors.push_back(i);
-                n/=i;
+                n /= i;
             }
         }
-        if(n>1) factors.push_back(n);
+        if (n > 1)
+            factors.push_back(n);
         return factors;
     }
 
-    int mySqrt(int x) {
+    int mySqrt(int x)
+    {
         vector<int> factors = PrimeFactors(x);
         int result = 1;
     }
 };
 
-int main(){
+int main()
+{
 
     return 0;
 }
