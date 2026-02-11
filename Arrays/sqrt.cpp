62153ca736c23e15661a08c85731787051e39d8e
shahbhavya7 <146073814+shahbhavya7@users.noreply.github.com>
2026-02-11 23:57:19 +0530

    chore

--- Arrays/sqrt.cpp
+++ Arrays/sqrt.cpp
@@ -4,21 +4,7 @@ using namespace std;
 class Solution
 {
 public:
-    vector<int> PrimeFactors(int n)
-    {
-        vector<int> factors;
-        for (int i = 2; i * i <= n; i++)
-        {
-            while (n % i == 0)
-            {
-                factors.push_back(i);
-                n /= i;
-            }
-        }
-        if (n > 1)
-            factors.push_back(n);
-        return factors;
-    }
+    
 
     int mySqrt(int x)
     {
