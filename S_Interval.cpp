56738752a369b8c91fa82ee42193371a5f7bda9f
Bhavya <phantomthunder77@gmail.com>
2026-02-17 10:35:40 +0530

    chore

--- S_Interval.cpp
+++ S_Interval.cpp
@@ -4,23 +4,30 @@ using namespace std;
 #define ll long long
 #define pb push_back
 
-void solve() {
+void solve()
+{
     float n;
-    cin>> n;
-    if(n>=0 && n<=25) cout << "Interval [0,25]" << endl;
-    else if(n>25 && n<=50) cout << "Interval (25,50]" << endl;
-    else if(n>50 && n<=75) cout << "Interval (50,75]" << endl;
-    else if(n>75 && n<=100) cout << "Interval (75,100]" << endl;
-    else cout << "Out of Intervals" << endl;
+    cin >> n;
+    if (n >= 0 && n <= 25)
+        cout << "Interval [0,25]" << endl;
+    else if (n > 25 && n <= 50)
+        cout << "Interval (25,50]" << endl;
+    else if (n > 50 && n <= 75)
+        cout << "Interval (50,75]" << endl;
+    else if (n > 75 && n <= 100)
+        cout << "Interval (75,100]" << endl;
+    else
+        cout << "Out of Intervals" << endl;
 }
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     // int t;
     // cin >> t;
     // while (t--) {
-        solve();
+    solve();
     // }
     return 0;
 }
