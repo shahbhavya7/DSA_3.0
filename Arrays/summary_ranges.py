7877ff85722bd36af47568e0a3f03831b25a69c6
shahbhavya7 <146073814+shahbhavya7@users.noreply.github.com>
2026-04-16 13:01:09 +0530

    summary Ranges

--- Arrays/summary_ranges.py
+++ Arrays/summary_ranges.py
@@ -4,8 +4,19 @@ class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
         final = []
         a, b = 0, 1
-        while b < len(nums):
-            if nums[b] - nums[a] == 1:
+        for i in range(len(nums)):
+            if b < len(nums) and nums[b] - nums[i] == 1:
                 b += 1
             else:
-                pass
+                if a == b - 1:
+                    final.append(str(nums[a]))
+                else:
+                    final.append(str(nums[a]) + "->" + str(nums[b - 1]))
+                a = b
+                b += 1
+        return final
+               
+    
+# Example usage:
+solution = Solution()
+print(solution.summaryRanges([0,1,2,4,5,7]))  # Output: ["0->2","4->5","7"]
