b77278e892974d6cdef76ee46763e359349e1fd7
shahbhavya7 <146073814+shahbhavya7@users.noreply.github.com>
2026-07-15 23:58:02 +0530

    permutation

---
+++ Recursion/permuatation.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    
+    def backtrack(self, nums, idx, res):
+        if idx == len(nums):
+            res.append(nums.copy())
+            return
+        
+        for i in range(idx, len(nums)):
+            nums[idx], nums[i] = nums[i], nums[idx]
+            self.backtrack(nums, idx + 1, res)
+            nums[idx], nums[i] = nums[i], nums[idx]
+        
+        
+    
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        self.backtrack(nums, 0, res)
+        return res
+    
+# Example usage
+solution = Solution()
+
+nums = [1, 2, 3]
+result = solution.permute(nums)
+print(f"The permutations of {nums} are: {result}")
